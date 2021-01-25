b6ee82d99db0eda02fb2f7ffec32ae5c31f3e18f
Duke <lkccomander@gmail.com>
2021-01-24 20:51:45 -0600

    Lambda

--- mod_3_week2.py
+++ mod_3_week2.py
@@ -49,3 +49,20 @@ print(list(abbrevs_upper))
 
 
 
+lst_check = ['plums', 'watermelon', 'kiwi', 'strawberries', 'blueberries', 'peaches', 'apples', 'mangos', 'papaya']
+filter_testing = list(filter(lambda word: 'w' in word, lst_check))
+print("Words that have a w in them:\n{}".format(filter_testing))
+
+
+things = [3, 4, 6, 7, 0, 1]
+#chaining together filter and map:
+# first, filter to keep only the even numbers
+# double each of them
+print(map(lambda x: x*2, filter(lambda y: y % 2 == 0, things)))
+
+# equivalent version using list comprehension
+print([x*2 for x in things if x % 2 == 0])
+
+
+
+
